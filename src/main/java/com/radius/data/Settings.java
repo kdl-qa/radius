0f520669c63fdf510c29bf54de9df125499133dd
kdl <testqa.kdl@gmail.com>
2016-02-26 18:37:25 +0200

    edit profile locators

---
+++ src/main/java/com/radius/data/Settings.java
@@ -0,0 +1,61 @@
+package com.radius.data;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+/**
+ * Created by kdl on 26.02.16.
+ */
+public class Settings {
+
+    /*===================== Common =====================*/
+
+    //todo: xpath Main and public blocks (class = android.widget.TextView)
+
+
+    @FindBy(id = "exit_button")
+    public static WebElement logoutBtn;
+
+    /*===================== Main Profile =====================*/
+
+    @FindBy(id = "image_view_avatar_main")
+    public static WebElement myMainAvatar;
+
+    @FindBy(id = "text_view_main_name")
+    public static WebElement myMainName;
+
+    @FindBy(id = "main_edit")
+    public static WebElement mainEditBtn;
+
+    /*==================== Public Profile ====================*/
+
+    @FindBy(id = "image_view_avatar_public")
+    public static WebElement myPublicAvatar;
+
+    @FindBy(id = "text_view_public_name")
+    public static WebElement myPublicName;
+
+    @FindBy(id = "public_edit")
+    public static WebElement publicEditBtn;
+
+    /*====================== Edit Profile =========================*/
+
+    @FindBy(id = "avatar_picker")
+    public static WebElement avatar_imageBtn;
+
+    @FindBy(id = "avatar_camera")
+    public static WebElement avatar_cameraBtn;
+
+    @FindBy(id = "delete_avatar")
+    public static WebElement avatar_deleteBtn;
+
+    @FindBy(id = "username_text_with_edit")
+    public static WebElement edit_usernameInput;
+
+    @FindBy(id = "profile_update_fab")
+    public static WebElement update_profileBtn;
+
+
+
+
+}
