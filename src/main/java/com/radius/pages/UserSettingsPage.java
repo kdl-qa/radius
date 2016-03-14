7e7775f792f0f321d2e7955fd99627017927d9f7
kdl <testqa.kdl@gmail.com>
2016-03-14 14:02:31 +0200

    refactor UserProfilePage

--- src/main/java/com/radius/pages/UserSettingsPage.java
+++ src/main/java/com/radius/pages/UserSettingsPage.java
@@ -1,6 +1,5 @@
 package com.radius.pages;
 
-import com.radius.data.Accounts;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.touch.TouchActions;
@@ -17,58 +16,96 @@ public class UserSettingsPage extends MobilePage {
 
     //todo: xpath Main and public blocks (class = android.widget.TextView)
 
-
+    /**
+     * logout button
+     */
     @FindBy(id = "exit_button")
     public static WebElement logoutBtn;
 
     /*----------------------------- Main Profile -------------------------------------------------*/
 
+    /**
+     * main profile avatar
+     */
     @FindBy(id = "image_view_avatar_main")
     public static WebElement myMainAvatar;
 
+    /**
+     * main profile username
+     */
     @FindBy(id = "text_view_main_name")
     public static WebElement myMainName;
 
+    /**
+     * main edit profile button
+     */
     @FindBy(id = "main_edit")
     public static WebElement mainProfileEditBtn;
 
     /*------------------------------ Public Profile -----------------------------------------------*/
 
+    /**
+     * public profile avatar
+     */
     @FindBy(id = "image_view_avatar_public")
     public static WebElement myPublicAvatar;
 
+    /**
+     * public profile username
+     */
     @FindBy(id = "text_view_public_name")
     public static WebElement myPublicName;
 
+    /**
+     * public edit profile button
+     */
     @FindBy(id = "public_edit")
     public static WebElement publicProfileEditBtn;
 
     /*---------------------------------- Edit Profile -------------------------------------------*/
 
+    /**
+     * image gallery picker
+     */
     @FindBy(id = "avatar_picker")
     public static WebElement avatar_imageBtn;
 
+    /**
+     * camera picker
+     */
     @FindBy(id = "avatar_camera")
     public static WebElement avatar_cameraBtn;
 
+    /**
+     * delete profile avatar button
+     */
     @FindBy(id = "delete_avatar")
     public static WebElement avatar_deleteBtn;
 
+    /**
+     * edit username field
+     */
     @FindBy(id = "username_text_with_edit")
     public static WebElement edit_usernameInput;
 
+    /**
+     * update profile button
+     */
     @FindBy(id = "profile_update_fab")
     public static WebElement update_profileBtn;
 
     /*--------------------------------- Image & Gallery -----------------------------------------*/
 
+    /**
+     * submit new avatar button
+     */
     @FindBy(id = "action_back_to_list")
     public static WebElement submitAvatarAction;
 
 
     /**
-    * Constructor
-    */
+     * Constructor
+     */
     public UserSettingsPage(AndroidDriver driver, WebDriverWait wait, TouchActions touchScreen) {
         super(driver, wait, touchScreen);
     }
@@ -92,7 +129,6 @@ public class UserSettingsPage extends MobilePage {
 
     /**
      * Edit main profile
-     * @param
      */
     public boolean editMainUsername(String updateUsername) {
         wait.until(ExpectedConditions.visibilityOf(edit_usernameInput));
@@ -104,6 +140,9 @@ public class UserSettingsPage extends MobilePage {
         return true;
     }
 
+    /**
+     * Check Main username
+     */
     public boolean checkEditedAccountMainUsername(String username) {
         wait.until(ExpectedConditions.visibilityOf(myMainName));
         if (myMainName.getText().equalsIgnoreCase(username)) {
@@ -112,8 +151,11 @@ public class UserSettingsPage extends MobilePage {
         return true;
     }
 
+    /**
+     * Delete profile avatar
+     */
     public boolean deleteProfileAvatar() {
-        wait.until(ExpectedConditions.visibilityOf(Accounts.avatar_deleteBtn));
+        wait.until(ExpectedConditions.visibilityOf(avatar_deleteBtn));
         if (avatar_deleteBtn.isEnabled()) {
             avatar_deleteBtn.click();
             update_profileBtn.click();
@@ -121,6 +163,9 @@ public class UserSettingsPage extends MobilePage {
         return true;
     }
 
+    /**
+     * Update avatar from gallery
+     */
     public boolean chooseAvatarFromGallery(int album, int imgIndex) {
         if (!DialogAndGroupPage.imagesList.isEmpty()) {
             DialogAndGroupPage.imagesList.get(album).click();
@@ -131,8 +176,11 @@ public class UserSettingsPage extends MobilePage {
         return true;
     }
 
+    /**
+     * Change profile avatar
+     */
     public boolean addProfileAvatar(int album, int img_index) {
-        wait.until(ExpectedConditions.visibilityOf(Accounts.avatar_imageBtn));
+        wait.until(ExpectedConditions.visibilityOf(avatar_imageBtn));
         if (!avatar_deleteBtn.isEnabled()) {
             avatar_imageBtn.click();
             chooseAvatarFromGallery(album, img_index);
